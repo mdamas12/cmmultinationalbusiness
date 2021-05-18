9f904bea0e673b76a2105834a7cae8e3c05e91f5
Marcos Damas <marcosdamas12@gmail.com>
2021-05-18 12:38:40 -0400

    modificacion del controlador

--- controllers/main.js
+++ controllers/main.js
@@ -1,33 +1,52 @@
 var app = angular.module("app",[]);
 
 app.controller("mainController", function($scope,$http){
-    $scope.tittle="prueba ";
-
+    $scope.tittle="prueba";
+    $scope.loading = false;
+    $scope.name = "";
+    $scope.email = "";
+    $scope.phone = "";
+    $scope.message = "";
 
     $scope.sendmail = function(){
         
-        //alert($scope.name+" "+$scope.email+" "+$scope.phone);
-        //console.log($scope.username);
-    
+      //alert($scope.name+" "+$scope.email+" "+$scope.phone);
+      if (($scope.name == "" ) || ($scope.email == "") || ($scope.phone == "") || ($scope.message == "")){
+        swal({
+          title: "You must complete all the fields",
+          button: "OK",
+          dangerMode: true
+        });
+
+        return
+
+      }
+        $scope.loading = true;
            $http({
              method: 'POST',
-             //url: 'http://localhost:3000/emails/', 
-             url: 'http://mdprofessionalservice.com:3000/emails/',
+             url: 'http://localhost:3000/emails/', 
+            // url: 'http://mdprofessionalservice.com:3000/emails/',
              data:{
                    name:$scope.name,
                    email:$scope.email,
-                   phone: $scope.phone
+                   phone: $scope.phone,
+                   message: $scope.message
                   }
             //headers:{'Content-Type': 'application/x-www-form-urlencoded'},
           }).then(function(response) {
+            $scope.loading = false;
             swal({
-                title: "Genial!",
-                text: "Tu Email ha sido enviado!",
+                title: "Alright!",
+                text: "Your email has been sent!",
                 icon: "success",
                 button: "OK",
                 dangerMode: true
               });
-     ;
+            $scope.name = "";
+            $scope.email = "";
+            $scope.phone = "";
+            $scope.message = "";
+ 
           });
    
         }
